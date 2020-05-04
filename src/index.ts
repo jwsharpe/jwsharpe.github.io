f147db86bdf9a6da12dc9bdb4478ce5f34e52970
James Sharpe <jameswrightsharpe@gmail.com>
2020-05-03 21:58:02 -0400

    setup webpack & typescript

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import * as _ from 'lodash';
+
+// function component(): Element {
+//     const element = document.createElement('div');
+//     element.innerHTML = _.join(['hasdfi', 'world'], ' ');
+//     return element;
+//   }
+  
+  document.body.innerText = ' hi';
